0fdb9aa7a08a19dc7340094e8de90146da10b327
Warsnak, Richard <Richard.Warsnak@Verint.com>
2022-06-13 13:44:56 -0400

    Add isVerintAlive.

--- src/main/java/com/verint/engageapidemo/controller/BasicWebServiceController.java
+++ src/main/java/com/verint/engageapidemo/controller/BasicWebServiceController.java
@@ -3,10 +3,9 @@ package com.verint.engageapidemo.controller;
 import com.verint.engageapidemo.model.ThrowItBackModel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class BasicWebServiceController {
@@ -23,5 +22,17 @@ public class BasicWebServiceController {
     public ResponseEntity<ThrowItBackModel> throwItBack(@RequestBody String body) {
         ThrowItBackModel response = new ThrowItBackModel(body);
         return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    @PostMapping("isVerintAlive")
+    public ResponseEntity<String> isVerintAlive(@RequestParam String authToken) {
+        String url = "https://blue77.verint.training/wfo/user-mgmt-api/v1/employees";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Impact360AuthToken", authToken);
+        HttpEntity<String> request = new HttpEntity<>(headers);
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> authResponse = restTemplate.exchange(url, HttpMethod.HEAD, request, String.class);
+        return ResponseEntity.status(authResponse.getStatusCode()).body(authResponse.getBody());
     }
 }
